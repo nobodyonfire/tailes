a8716eb458dc1366b31c7e08a45363454d8abc6c
Alexis Soltysiak <65892122+nobodyonfire@users.noreply.github.com>
2020-12-11 11:29:41 +0100

    Add files via upload

---
+++ Game.java
@@ -0,0 +1,170 @@
+package java2dgame;
+
+
+import gfx.Screen;
+import gfx.SpriteSheet;
+import java.awt.BorderLayout;
+import java.awt.Canvas;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferInt;
+
+import javax.swing.JFrame;
+
+
+
+public class Game extends Canvas implements Runnable {
+
+    private static final long serialVersionUID= 1L;
+    
+    public static final int WIDTH = 160; 
+    public static final int HEIGHT = WIDTH/12*9;
+    public static final int SCALE =3;
+    public static final String NAME="Game";
+    
+    private JFrame frame;
+    
+    public boolean running = false;
+    public int tickCount=0;
+    
+    private BufferedImage image= new BufferedImage(WIDTH,HEIGHT,BufferedImage.TYPE_INT_RGB);
+    private int[] pixels = ((DataBufferInt)image.getRaster().getDataBuffer()).getData();
+    
+    private Screen screen;
+    public InputeHandler input;
+    
+    public Game(){
+        setMinimumSize(new Dimension(WIDTH * SCALE,HEIGHT * SCALE));
+        setMaximumSize(new Dimension(WIDTH * SCALE,HEIGHT * SCALE));
+        setPreferredSize(new Dimension(WIDTH * SCALE,HEIGHT * SCALE));
+        
+        frame = new JFrame(NAME);
+        
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+        
+        frame.add(this,BorderLayout.CENTER);
+        frame.pack();
+        
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+         
+        
+    }
+    
+    public void init(){
+        screen=new Screen(WIDTH,HEIGHT,new SpriteSheet("/sprite_sheet.png"));
+        input = new InputeHandler(this);
+    }
+     
+    public synchronized void start() {
+        running=true;
+        new Thread(this).start();
+    }
+    
+    public synchronized void stop() {
+        running = false;
+    }
+
+    
+
+    public void run() {  
+        
+        long lastTime = System.nanoTime();
+        double nsPerTick = 1000000000D/60D;
+        
+        int frames=0;
+        int ticks =0;
+        
+        long lastTimer=System.currentTimeMillis();
+        double delta = 0;
+        
+        init();
+        
+        while(running){
+            long now=System.nanoTime();
+            delta+=(now-lastTime)/nsPerTick;
+            lastTime = now;
+            boolean shouldRender =true;
+            
+            while(delta>=1){
+                ticks++;
+                tick();
+                delta-=1;
+                shouldRender= true;
+            }
+            
+            try{
+                Thread.sleep(2);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            } 
+            
+            if (shouldRender){
+                frames++;
+                render();
+                
+            }
+          
+            
+            if (System.currentTimeMillis()-lastTimer >1000){
+                lastTimer +=1000;
+                System.out.println(frames+ " "+ticks);
+                frames =0;
+                ticks =0;
+            }
+        }
+    }
+    
+      
+    public void tick(){
+        tickCount++;
+        
+        if (input.up.isPressed()){
+            screen.yOffset--;
+        }
+        if (input.down.isPressed()){
+            screen.yOffset++;
+        }
+        if (input.left.isPressed()){
+            screen.xOffset--;
+        }
+        if (input.right.isPressed()){
+            screen.xOffset++;
+        }
+
+
+         
+    }
+    
+    public void render(){
+        BufferStrategy bs=getBufferStrategy();
+        if (bs==null){
+            createBufferStrategy(3);
+            return;
+        }
+        
+        screen.render(pixels, 0, WIDTH);
+        
+        Graphics g = bs.getDrawGraphics();
+        g.drawImage(image,0,0,getWidth(),getHeight(),null);
+   
+        g.dispose();
+        bs.show();
+        
+    }
+    
+    
+    public static void main(String[] args) {
+        
+        new Game().start();
+        
+    }
+
+    
+    
+    
+}
