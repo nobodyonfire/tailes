948053e79a9b7333aa4ba924a777dd40ab67e29e
Alexis Soltysiak <65892122+nobodyonfire@users.noreply.github.com>
2020-12-11 15:08:52 +0100

    Add files via upload

--- java2dgame/Game.java
+++ java2dgame/Game.java
@@ -1,6 +1,8 @@
 package java2dgame;
 
 
+import gfx.Colours;
+import gfx.Font;
 import gfx.Screen;
 import gfx.SpriteSheet;
 import java.awt.BorderLayout;
@@ -31,6 +33,7 @@ public class Game extends Canvas implements Runnable {
     
     private BufferedImage image= new BufferedImage(WIDTH,HEIGHT,BufferedImage.TYPE_INT_RGB);
     private int[] pixels = ((DataBufferInt)image.getRaster().getDataBuffer()).getData();
+    private int[] colours =new int[6*6*6];
     
     private Screen screen;
     public InputeHandler input;
@@ -56,6 +59,20 @@ public class Game extends Canvas implements Runnable {
     }
     
     public void init(){
+        int index = 0;
+        for (int r = 0; r < 6; r++) {
+            for (int g = 0; g < 6; g++) {
+                for (int b = 0; b < 6; b++) {
+                    int rr = (r * 255 / 5);
+                    int gg = (g * 255 / 5);
+                    int bb = (b * 255 / 5);
+
+                    colours[index++] = rr << 16 | gg << 8 | bb;
+                }
+            }
+        }
+        
+        
         screen=new Screen(WIDTH,HEIGHT,new SpriteSheet("/sprite_sheet.png"));
         input = new InputeHandler(this);
     }
@@ -147,7 +164,24 @@ public class Game extends Canvas implements Runnable {
             return;
         }
         
-        screen.render(pixels, 0, WIDTH);
+        
+        for (int y = 0; y <32;y++){
+            for (int x = 0; x <32;x++){
+                screen.render(x<<3,y<<3,0,Colours.get(555,505,052,550),true,false);
+          }
+        }
+        
+        String msg ="Hello world 1";
+        Font.render(msg, screen, screen.xOffset, screen.yOffset , Colours.get(-1, -1, -1, 000));
+        
+        for (int y = 0; y < screen.height; y++) {
+            for (int x = 0; x < screen.width; x++) {
+                int colourCode = screen.pixels[x + y * screen.width];
+                if (colourCode < 255)
+                    pixels[x + y * WIDTH] = colours[colourCode];
+            }
+        }
+        
         
         Graphics g = bs.getDrawGraphics();
         g.drawImage(image,0,0,getWidth(),getHeight(),null);
--- java2dgame/InputeHandler.java
+++ java2dgame/InputeHandler.java
@@ -53,16 +53,16 @@ public class InputeHandler implements KeyListener{
     }
      
     public void toggleKey (int keyCode,boolean isPressed){
-        if (keyCode == KeyEvent.VK_Z){ 
+        if (keyCode == KeyEvent.VK_Z || keyCode == KeyEvent.VK_UP){ 
             up.toggle(isPressed);
         }
-        if (keyCode == KeyEvent.VK_S){ 
+        if (keyCode == KeyEvent.VK_S || keyCode == KeyEvent.VK_DOWN){ 
             down.toggle(isPressed);
         }
-        if (keyCode == KeyEvent.VK_Q){ 
+        if (keyCode == KeyEvent.VK_Q || keyCode == KeyEvent.VK_LEFT){ 
             left.toggle(isPressed);
         }
-        if (keyCode == KeyEvent.VK_D){ 
+        if (keyCode == KeyEvent.VK_D || keyCode == KeyEvent.VK_RIGHT){ 
             right.toggle(isPressed);
         }
         
