489dee3b4c44bd81026c22330eef185c98048c39
Alexis Soltysiak <65892122+nobodyonfire@users.noreply.github.com>
2020-12-12 15:00:10 +0100

    Update Level.java

--- Level/Level.java
+++ Level/Level.java
@@ -1,60 +1,79 @@
 package Level;
 
-import gfx.Colours;
+import entities.Entity;
 import gfx.Screen;
+import java.util.ArrayList;
+import java.util.List;
 import tiles.Tile;
 
 public class Level {
  
-	private byte[] tiles;
-	public int width;
-	public int height;
- 
-	public Level(int width, int height) {
-		tiles = new byte[width * height];
-		this.width = width;
-		this.height = height;
-		this.generateLevel();
-	}
- 
-	public void generateLevel() {
-		for (int y = 0; y < height; y++) {
-			for (int x = 0; x < width; x++) {
-				if (x * y % 10 < 5) {
-					tiles[x + y * width] = Tile.GRASS.getId();
-				} else {
-					tiles[x + y * width] = Tile.STONE.getId();
-				}
-			}
-		}
-	}
- 
-	public void tick() {
- 
-	}
- 
-	public void renderTiles(Screen screen, int xOffset, int yOffset) {
-		if (xOffset < 0)
-			xOffset = 0;
-		if (xOffset > ((width << 3) - screen.width))
-			xOffset = ((width << 3) - screen.width);
-		if (yOffset < 0)
-			yOffset = 0;
-		if (yOffset > ((height << 3) - screen.height))
-			yOffset = ((height << 3) - screen.height);
- 
-		screen.setOffset(xOffset, yOffset);
- 
-		for (int y = (yOffset >> 3); y < (yOffset + screen.height >> 3) + 1; y++) {
-			for (int x = (xOffset >> 3); x < (xOffset + screen.width >> 3) + 1; x++) {
-				getTile(x, y).render(screen, this, x << 3, y << 3);
-			}
-		}
-	}
+    private byte[] tiles;
+    public int width;
+    public int height;
+
+    public List<Entity> entities = new ArrayList<Entity>();
+
+
+
+
+    public Level(int width, int height) {
+            tiles = new byte[width * height];
+            this.width = width;
+            this.height = height;
+            this.generateLevel();
+    }
+
+    public void generateLevel() {
+            for (int y = 0; y < height; y++) {
+                    for (int x = 0; x < width; x++) {
+                            if (x * y % 10 < 5) {
+                                    tiles[x + y * width] = Tile.GRASS.getId();
+                            } else {
+                                    tiles[x + y * width] = Tile.STONE.getId();
+                            }
+                    }
+            }
+    }
+
+    public void tick() {
+        for (Entity e : entities){
+            e.tick();
+        }
+    }
+
+    public void renderTiles(Screen screen, int xOffset, int yOffset) {
+            if (xOffset < 0)
+                    xOffset = 0;
+            if (xOffset > ((width << 3) - screen.width))
+                    xOffset = ((width << 3) - screen.width);
+            if (yOffset < 0)
+                    yOffset = 0;
+            if (yOffset > ((height << 3) - screen.height))
+                    yOffset = ((height << 3) - screen.height);
+
+            screen.setOffset(xOffset, yOffset);
+
+            for (int y = (yOffset >> 3); y < (yOffset + screen.height >> 3) + 1; y++) {
+                    for (int x = (xOffset >> 3); x < (xOffset + screen.width >> 3) + 1; x++) {
+                            getTile(x, y).render(screen, this, x << 3, y << 3);
+                    }
+            }
+    }
+        
+    public void renderEntities(Screen screen){
+        for (Entity e : entities){
+            e.render(screen);
+        }
+    }
  
     public Tile getTile(int x, int y) {
-        if (0 > x || x >= width || 0 > y || y >= height)
+        if (0 >  x || x >= width || 0 > y || y >= height)
             return Tile.VOID;
         return Tile.tiles[tiles[x + y * width]];
+    }
+    
+    public void addEntity(Entity entity){
+        this.entities.add(entity);
     }
 }
