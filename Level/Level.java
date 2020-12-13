58f0c861719d464def9b0079507e1508b05d82b3
Alexis Soltysiak <65892122+nobodyonfire@users.noreply.github.com>
2020-12-13 13:27:35 +0100

    Add files via upload

--- Level/Level.java
+++ Level/Level.java
@@ -2,8 +2,12 @@ package Level;
 
 import entities.Entity;
 import gfx.Screen;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import javax.imageio.ImageIO;
 import tiles.Tile;
 
 public class Level {
@@ -13,21 +17,72 @@ public class Level {
     public int height;
 
     public List<Entity> entities = new ArrayList<Entity>();
+    private String imagePath;
+    private BufferedImage image;
+    
 
 
 
-
-    public Level(int width, int height) {
+    public Level(String imagePath) {
+        
+        if (imagePath != null){
+            this.imagePath=imagePath;
+            this.loadLevelFromFile();
+        } else {
             tiles = new byte[width * height];
-            this.width = width;
-            this.height = height;
+            this.width = 64;
+            this.height = 64;
             this.generateLevel();
+            
+        }
+   
+    }
+    
+    private void loadLevelFromFile(){
+        try{
+            this.image = ImageIO.read(Level.class.getResource(this.imagePath));
+            this.width= image.getWidth();
+            this.height=image.getHeight();
+            tiles = new byte [width * height];
+            this.loadTiles();
+            
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+    
+    private void loadTiles(){
+        int[] tileColours = this.image.getRGB(0 ,0 ,width , height, null , 0 ,width);
+        for (int y = 0 ; y < height ; y ++){
+            for (int x = 0; x < width ; x ++) {
+                tileCheck : for (Tile t : Tile.tiles){
+                    if(t!=null && t.getlevelColour() == tileColours[x + y*width]){
+                        this.tiles[x + y * width]= t.getId();
+                        break tileCheck;
+                    }
+                }
+            }
+        }
+    }
+    
+    private void saveLeveltoFile(){
+        try {
+            ImageIO.write(image,"png",new File(Level.class.getResource(this.imagePath).getFile()));
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+    
+    public void alterTile(int x , int y , Tile newTile){
+        this.tiles[x + y*width] = newTile.getId();
+        image.setRGB(x, y, newTile.getlevelColour());
+        
     }
 
     public void generateLevel() {
             for (int y = 0; y < height; y++) {
                     for (int x = 0; x < width; x++) {
-                            if (x * y % 10 < 5) {
+                            if (x * y % 10 < 7) {
                                     tiles[x + y * width] = Tile.GRASS.getId();
                             } else {
                                     tiles[x + y * width] = Tile.STONE.getId();
@@ -39,6 +94,13 @@ public class Level {
     public void tick() {
         for (Entity e : entities){
             e.tick();
+        }
+        
+        for (Tile t : Tile.tiles){
+            if(t == null){
+                break;
+            }
+            t.tick();
         }
     }
 
@@ -68,7 +130,7 @@ public class Level {
     }
  
     public Tile getTile(int x, int y) {
-        if (0 >  x || x >= width || 0 > y || y >= height)
+        if (0 > x || x >= width || 0 > y || y >= height)
             return Tile.VOID;
         return Tile.tiles[tiles[x + y * width]];
     }
