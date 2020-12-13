b2ccd4c341f628ba226a75581a7f8ee4e4f72ede
Alexis Soltysiak <65892122+nobodyonfire@users.noreply.github.com>
2020-12-13 13:29:37 +0100

    Add files via upload

--- tiles/BasicTile.java
+++ tiles/BasicTile.java
@@ -8,14 +8,18 @@ public class BasicTile extends Tile {
 	protected int tileId;
 	protected int tileColour;
  
-	public BasicTile(int id, int x, int y, int tileColour) {
-		super(id, false, false);
-		this.tileId = x + y;
+	public BasicTile(int id, int x, int y, int tileColour,int levelColour) {
+		super(id, false, false, levelColour);
+		this.tileId = x + y * 32;
 		this.tileColour = tileColour;
 	}
+        
+        public void tick(){
+            
+        }
  
 	public void render(Screen screen, Level level, int x, int y) {
-		screen.render(x, y, tileId, tileColour);
+		screen.render(x, y, tileId, tileColour,0x00,1);
 	}
  
 }
