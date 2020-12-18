c5527ca261ad83bd581d8d06c23c3ad26ad77e08
Alexis Soltysiak <65892122+nobodyonfire@users.noreply.github.com>
2020-12-18 18:15:23 +0100

    Add files via upload

--- src/tiles/AnimatedSolidTile.java
+++ src/tiles/AnimatedSolidTile.java
@@ -4,19 +4,20 @@ import Level.Level;
 import gfx.Screen;
 
 
-public class AnimatedTile extends BasicTile {
+public class AnimatedSolidTile extends BasicTile {
 
     private int[][] animationTileCoords;
     private int currentAnimationIndex;
     private long lastIterationTime;
     private int animationSwitchDelay;
 
-    public AnimatedTile(int id, int[][] animationCoords, int levelColour, int animationSwitchDelay) {
+    public AnimatedSolidTile(int id, int[][] animationCoords, int levelColour, int animationSwitchDelay) {
         super(id, animationCoords[0][0], animationCoords[0][1], levelColour);
         this.animationTileCoords = animationCoords;
         this.currentAnimationIndex = 0;
         this.lastIterationTime = System.currentTimeMillis();
         this.animationSwitchDelay = animationSwitchDelay;
+        this.solid= true;
     }
 
     public void tick() {
@@ -24,6 +25,7 @@ public class AnimatedTile extends BasicTile {
             lastIterationTime = System.currentTimeMillis();
             currentAnimationIndex = (currentAnimationIndex + 1) % animationTileCoords.length;
             this.tileId = (animationTileCoords[currentAnimationIndex][0] + (animationTileCoords[currentAnimationIndex][1] * 32));
+            System.out.println(tileId);
           
         }
     }
--- src/tiles/Tile.java
+++ src/tiles/Tile.java
@@ -6,19 +6,81 @@ import gfx.Screen;
 
 public abstract class Tile {
  
-	public static final Tile[] tiles = new Tile[256];
+	public static final Tile[] tiles = new Tile[512];
 	public static final Tile VOID = new BasicSolidTile(0, 0, 0,0xFF000000);
 	public static final Tile STONE = new BasicSolidTile(1, 1, 0,0xFF555555);
 	public static final Tile GRASS = new BasicTile(2, 2, 0,0xFF00FF00);
-        public static final Tile WATER = new AnimatedTile(3,new int[][]{{0,5},{0,5},{0,5},{1,5},{2,5},{3,5},{4,5},{5,5},{6,5},{7,5},{6,5},{5,5},{4,5},{3,5},{2,5},{1,5}},0xFF0000FF,130);
         
-	protected byte id;
+        
+        public static final Tile WATER1 = new AnimatedTile(3,new int[][]{{0,5},{0,5},{0,5},{1,5},{2,5},{3,5},{2,5},{1,5}},0xFF0000FF,200);
+        public static final Tile WATER = new BasicTile(4,0,5,0xFF0000FE);
+        public static final Tile Grass1 = new BasicTile(5,4,0,0xFF00FE00);
+                
+        
+        public static final Tile lamptop = new AnimatedSolidTile(256,new int[][]{{2,7},{3,7},{4,7}},0xFF0A0000,200);
+        public static final Tile lampbottom = new BasicSolidTile(11, 2, 8,0xFF0B0000);
+        
+        
+        public static final Tile Cliff1 = new BasicSolidTile(100,3,0,0xFF010000);
+        public static final Tile Cliff2 = new BasicSolidTile(101,5,0,0xFF010001);
+        public static final Tile Cliffendup = new BasicSolidTile(102,3,1,0xFF020000);
+        public static final Tile Cliffendleft = new BasicSolidTile(103,4,1,0xFF030000);
+        public static final Tile Cliffend= new BasicSolidTile(104,2,1,0xFF040000);
+        
+        public static final Tile roadhorizontal1 = new BasicTile(64,0,2,0xFF400000); /*  [64/0/0]  */
+        public static final Tile roadhorizontal2 = new BasicTile(65,1,2,0xFF410000); /*  [65/0/0]  */
+        
+        
+        public static final Tile roadvertical1 = new BasicTile(66,2,2,0xFF420000); /*  [66/0/0]  */
+        public static final Tile roadvertical2 = new BasicTile(67,3,2,0xFF430000); /*  [67/0/0]  */
+        
+        public static final Tile roadcrossings = new BasicTile(68,4,2,0xFF440000); /*  [68/0/0]  */
+        public static final Tile roadcrossingn = new BasicTile(69,5,2,0xFF450000); /*  [69/0/0]  */
+        public static final Tile roadcrossingo = new BasicTile(70,6,2,0xFF460000); /*  [70/0/0]  */
+        public static final Tile roadcrossinge = new BasicTile(71,7,2,0xFF470000); /*  [71/0/0]  */
+        
+        public static final Tile pavedroadhorizontal1 = new BasicTile(96,0,3,0xFF600000); /*  [96/0/0]  */
+        public static final Tile pavedroadhorizontal2 = new BasicTile(97,1,3,0xFF610000); /*  [97/0/0]  */
+        
+        public static final Tile pavedroadvertical1 = new BasicTile(98,2,3,0xFF620000); /*  [98/0/0]  */
+        public static final Tile pavedroadvertical2 = new BasicTile(99,3,3,0xFF630000); /*  [99/0/0]  */
+        
+        public static final Tile pavedroadhorizontalup1 = new BasicTile(110,0,4,0xFF800000); /*  [128/0/0]  */
+        public static final Tile pavedroadhorizontalup2 = new BasicTile(111,1,4,0xFF810000); /*  [129/0/0]  */
+        
+        public static final Tile pavedroadhorizontaldown1 = new BasicTile(112,2,4,0xFF820000); /*  [130/0/0]  */
+        public static final Tile pavedroadhorizontaldown2 = new BasicTile(113,3,4,0xFF830000); /*  [131/0/0]  */
+        
+        public static final Tile pavedroadverticalleft1 = new BasicTile(114,4,4,0xFF840000); /*  [132/0/0]  */
+        public static final Tile pavedroadverticalleft2 = new BasicTile(115,5,4,0xFF850000); /*  [133/0/0]  */
+        
+        public static final Tile pavedroadverticalright1 = new BasicTile(116,6,4,0xFF860000); /*  [134/0/0]  */
+        public static final Tile pavedroadverticalright2 = new BasicTile(117,7,4,0xFF870000); /*  [135/0/0]  */
+        
+        public static final Tile pavedroadcoin1 = new BasicTile(118,4,3,0xFF880000); /*  [136/0/0]  */
+        public static final Tile pavedroadcoin2 = new BasicTile(119,7,3,0xFF890000); /*  [137/0/0]  */
+        public static final Tile pavedroadcoin3 = new BasicTile(120,5,3,0xFF8A0000); /*  [138/0/0]  */
+        public static final Tile pavedroadcoin4 = new BasicTile(121,6,3,0xFF8B0000); /*  [139/0/0]  */
+        
+        public static final Tile pavedroadcoininv1 = new BasicTile(122,8,4,0xFF8C0000); /*  [140/0/0]  */
+        public static final Tile pavedroadcoininv2 = new BasicTile(123,9,4,0xFF8D0000); /*  [141/0/0]  */
+        public static final Tile pavedroadcoininv3 = new BasicTile(124,10,4,0xFF8E0000); /*  [142/0/0]  */
+        public static final Tile pavedroadcoininv4 = new BasicTile(125,11,4,0xFF8F0000); /*  [143/0/0]  */
+        
+        
+        
+  
+        
+        
+        
+        
+	protected int id;
 	protected boolean solid;
     	protected boolean emitter;
         private int levelColour;
         
 	public Tile(int id, boolean isSolid, boolean isEmitter,int levelColour) {
-		this.id = (byte) id;
+		this.id = id;
 		if (tiles[id] != null) {
 			throw new RuntimeException("Duplicate tile id on" + id);
 		}
@@ -28,7 +90,7 @@ public abstract class Tile {
 		tiles[id] = this;
 	}
  
-	public byte getId() {
+	public int getId() {
 		return id;
 	}
  
