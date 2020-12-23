9f79367b60ef708dd4d136a10a3615247d047a00
Alexis Soltysiak <65892122+nobodyonfire@users.noreply.github.com>
2020-12-23 18:06:22 +0100

    Add files via upload

--- src/Level/Level.java
+++ src/Level/Level.java
@@ -95,13 +95,19 @@ public class Level {
         for (Entity e : entities){
             e.tick();
         }
+         
         
         for (Tile t : Tile.tiles){
 
             if(t != null){
                  t.tick();
             } 
-            
+        }
+    }
+    
+    public void tickattack(Screen screen){
+        for (Entity e : entities){
+            e.tickattack(screen);
         }
     }
 
@@ -123,20 +129,65 @@ public class Level {
                     }
             }
     }
+    
+    
+    public void renderTilesUP(Screen screen, int xOffset, int yOffset) {
+            if (xOffset < 0)
+                    xOffset = 0;
+            if (xOffset > ((width << 5) - screen.width))       /*  BORDURES */
+                    xOffset = ((width << 5) - screen.width);    /*  BORDURES */
+            if (yOffset < 0)
+                    yOffset = 0;
+            if (yOffset > ((height << 5) - screen.height))      /*  BORDURES */
+                    yOffset = ((height << 5) - screen.height);      /*  BORDURES */
+
+            screen.setOffset(xOffset, yOffset);
+
+            for (int y = (yOffset >> 5); y < (yOffset + screen.height >> 5) + 1; y++) {  /* TAILLE DU RENDU EN LONGUEUR */
+                    for (int x = (xOffset >> 5); x < (xOffset + screen.width >> 5) + 1; x++) {  /* TAILLE DU RENDU EN LARGEUR */
+                            getTileUP(x, y).render(screen, this, x << 5, y << 5);
+                    }
+            }
+    }
         
     public void renderEntities(Screen screen){
         for (Entity e : entities){
             e.render(screen);
         }
     }
+    
+    public void attackEntities(Screen screen,String name, int xMin , int xMax, int yMin, int yMax, int damage) {
+        for (Entity e : entities){
+            e.isAttacked( name,  xMin ,  xMax, yMin,yMax, damage );
+        }
+    }
  
     public Tile getTile(int x, int y) {
-        if (0 > x || x >= width || 0 > y || y >= height)
+        if (0 > x || x >= width || 0 > y || y >= height  )
+            return Tile.VOID;
+        if(Tile.tiles[tiles[x + y * width]].isEmitter()== true){
+            if (Tile.tiles[tiles[x + y * width]].getId()>235 || Tile.tiles[tiles[x + y * width]].getId()<200 ){
+                return Tile.Grass1;
+            } else {
+                return Tile.pavedroadhorizontal1;
+            }     
+        } 
+            
+        return Tile.tiles[tiles[x + y * width]];
+    }
+    
+    public Tile getTileUP(int x, int y) {
+        if ((0 > x || x >= width || 0 > y || y >= height) )
+            return Tile.VOID;
+        if (Tile.tiles[tiles[x + y * width]].isEmitter()== false)
             return Tile.VOID;
         return Tile.tiles[tiles[x + y * width]];
     }
     
+    
     public void addEntity(Entity entity){
         this.entities.add(entity);
     }
+
+  
 }
