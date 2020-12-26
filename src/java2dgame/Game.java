e4d330855f44c6c9c605a2c676df759e18a61e13
Alexis Soltysiak <65892122+nobodyonfire@users.noreply.github.com>
2020-12-26 01:21:51 +0100

    Add files via upload

--- src/java2dgame/Game.java
+++ src/java2dgame/Game.java
@@ -88,6 +88,7 @@ public class Game extends Canvas implements Runnable {
                 level.addEntity(player);
                 level.addEntity(Cat);
              
+             
                 
 	} 
  
